b6fc97d800d3eb8457d6a893da9dec9d2d1359ac
Renan Marangon <maracunha@gmail.com>
2023-04-13 12:05:46 -0300

    fix: Type error

--- src/components/InfoCard/InfoCard.test.tsx
+++ src/components/InfoCard/InfoCard.test.tsx
@@ -7,9 +7,14 @@ expect.extend(matchers);
 import InfoCard from './index';
 
 test('displays a green and in Operation status', async () => {
-  const card = render(
-    <InfoCard status={{}}/>,
-  );
+  const status = {
+    inOperation: 0,
+    inAlert: 0,
+    plannedStop: 0,
+    notplannetStop: 0,
+    inDowntime: 0,
+  };
+  const card = render(<InfoCard status={status} />);
 
   expect(screen.getByText(/no data/i)).toBeInTheDocument();
   card.unmount();
