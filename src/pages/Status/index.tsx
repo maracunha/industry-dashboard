e1fbf08cfae56ef6b656f2ec80d9463190b9ffb0
Renan Marangon <maracunha@gmail.com>
2023-04-13 11:15:08 -0300

    refactor: Remove not used code

--- src/pages/Status/index.tsx
+++ src/pages/Status/index.tsx
@@ -2,7 +2,6 @@ import { useLocation } from 'react-router-dom';
 import { Avatar, Card, Descriptions, Empty, Tooltip } from 'antd';
 
 import { useAssetsList } from '../../services/queries';
-import styles from './styles.module.css';
 import { optionsChartGauge, optionsChartPie } from './chartsHelper';
 import Chart from '../../components/Chart';
 import { useUSerAvatar } from '../../hooks/useUserAvatar';
@@ -25,14 +24,14 @@ function Status() {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-8">
       <div className="md:grid-span-1">
         <Card cover={<img alt="example" src={asset.image} />}>
-          <Descriptions title={asset.name} column={1} className={styles.description}>
+          <Descriptions title={asset.name} column={1}>
             <Descriptions.Item label="Model: ">{asset.model}</Descriptions.Item>
             <Descriptions.Item label="Sensors: ">{asset.sensors[0]}</Descriptions.Item>
             <Descriptions.Item label="Max Temp: ">
               {asset.specifications.maxTemp} c
             </Descriptions.Item>
             <Descriptions.Item label="Healh Score: ">{asset.healthscore} %</Descriptions.Item>
-            <Descriptions.Item label="Assigned Users" className={styles.assigned}>
+            <Descriptions.Item label="Assigned Users">
               {avatars.map((a) => (
                 <Tooltip key={`${asset.id}-${a}`} title={a} placement="top" className="cursor-help">
                   <Avatar className="mr-2">{a.charAt(0)}</Avatar>
